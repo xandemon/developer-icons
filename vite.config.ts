9e0412585c517d93c8f36363fc61dcafe6413211
Sandesh Katwal <dev01.sandesh@gmail.com>
2024-05-01 18:52:17 +0545

    :star: new: initialize vite with react and typescript

---
+++ vite.config.ts
@@ -0,0 +1,14 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  build: {
+    lib: {
+      entry: resolve(__dirname, "lib/main.ts"),
+      formats: ["es"],
+    },
+  },
+});
