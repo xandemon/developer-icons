7f1d5ac7518c5f9cf2425d5e6b662dfd0e727b3d
Sandesh Katwal <dev01.sandesh@gmail.com>
2024-05-03 00:10:29 +0545

    :wrench: config: modify configs to make exporting components work

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,20 @@
 import { defineConfig } from "vite";
-import { resolve } from "path";
+import dts from "vite-plugin-dts";
+import { libInjectCss } from "vite-plugin-lib-inject-css";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), libInjectCss(), dts({ include: ["lib"] })],
   build: {
+    copyPublicDir: false,
     lib: {
-      entry: resolve(__dirname, "lib/main.ts"),
+      entry: resolve(__dirname, "lib/index.ts"),
       formats: ["es"],
+    },
+    rollupOptions: {
+      external: ["react", "react/jsx-runtime"],
     },
   },
 });
