9742a904dc10192cb3bbfdd12ee6c23e40ebcec9
Sandesh Katwal <dev01.sandesh@gmail.com>
2024-05-03 00:07:14 +0545

    :star: feat: create icons folder for generated icon components with dummy icons list

---
+++ lib/icons/icons.data.ts
@@ -0,0 +1,39 @@
+const IconsData = [
+  {
+    id: "html",
+    name: "HTML",
+    path: "public/html.svg",
+    categories: ["web", "frontend"],
+    keywords: ["html", "html5", "markup", "language"],
+  },
+  {
+    id: "canva",
+    name: "Canva",
+    path: "public/canva.svg",
+    categories: ["web", "frontend"],
+    keywords: ["html", "html5", "markup", "language"],
+  },
+  {
+    id: "css",
+    name: "CSS",
+    path: "public/css.svg",
+    categories: ["web", "frontend"],
+    keywords: ["html", "html5", "markup", "language"],
+  },
+  {
+    id: "discord",
+    name: "Discord",
+    path: "public/discord.svg",
+    categories: ["web", "frontend"],
+    keywords: ["html", "html5", "markup", "language"],
+  },
+  {
+    id: "figma",
+    name: "Figma",
+    path: "public/figma.svg",
+    categories: ["web", "frontend"],
+    keywords: ["html", "html5", "markup", "language"],
+  },
+];
+
+export default IconsData;
