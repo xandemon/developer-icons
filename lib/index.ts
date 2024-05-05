710b799e7145a61df9724d7d04c805e493a33bcd
Sandesh Katwal <dev01.sandesh@gmail.com>
2024-05-05 23:49:38 +0545

    :star: new: create a util function to capitalize first letter

--- lib/index.ts
+++ lib/index.ts
@@ -1,3 +1,4 @@
 export * from "./icons";
 export * from "./createDeveloperIcon";
+export * from "./utils/capitalizeFirstLetter";
 export { default as IconsData } from "./icons/icons.data";
---
+++ lib/utils/capitalizeFirstLetter.ts
@@ -0,0 +1,2 @@
+export const capitalizeFirstletter = (str: string) =>
+  `${str[0]}${str.slice(1).toLowerCase()}`;
