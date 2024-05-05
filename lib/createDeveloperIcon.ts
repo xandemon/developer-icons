76e618d7a07508b9042b8b78889a4f3c5020d759
Sandesh Katwal <dev01.sandesh@gmail.com>
2024-05-05 23:14:17 +0545

    :star: feat: dynamically export icon components that accept props for customization

--- lib/createDeveloperIcon.ts
+++ lib/createDeveloperIcon.ts
@@ -1,6 +1,18 @@
+import { createElement } from "react";
+import { parseSync, stringify } from "svgson";
+
 export const createDeveloperIcon = (iconName: string, iconContent: string) => {
-  return `export const ${iconName} = () => {
-    return ${iconContent}
-  }
-  `;
+  const svgObject = parseSync(iconContent, { camelcase: true });
+  const children = [...svgObject.children.map((child) => stringify(child))];
+
+  return `import { createElement } from 'react';
+  import {Icon} from '../createDeveloperIcon';
+  export const ${iconName} = (props) => createElement(Icon, {...${JSON.stringify(
+    svgObject.attributes
+  )}, ...props, children: [${children}]})`;
+};
+
+export const Icon = (props) => {
+  const svgString = createElement("svg", props);
+  return svgString;
 };
