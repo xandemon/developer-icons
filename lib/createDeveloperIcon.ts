d1dcf70eae69a4e67f3fdcc83f2d811c761a0aa5
Sandesh Katwal <dev01.sandesh@gmail.com>
2024-05-07 23:03:16 +0545

    :star: feat: add type support to icon props

--- lib/createDeveloperIcon.ts
+++ lib/createDeveloperIcon.ts
@@ -1,18 +1,35 @@
-import { createElement } from "react";
+import { SVGProps, createElement } from "react";
 import { parseSync, stringify } from "svgson";
+import defaultAttributes from "./defaultAttributes";
+import { mergeClassNames } from "./utils/mergeClassNames";
+
+export interface DeveloperIconProps extends Partial<SVGProps<SVGElement>> {
+  size?: number;
+}
 
 export const createDeveloperIcon = (iconName: string, iconContent: string) => {
   const svgObject = parseSync(iconContent, { camelcase: true });
   const children = [...svgObject.children.map((child) => stringify(child))];
 
-  return `import { createElement } from 'react';
-  import {Icon} from '../createDeveloperIcon';
-  export const ${iconName} = (props) => createElement(Icon, {...${JSON.stringify(
+  return `import { createElement } from 'react';\nimport {Icon, DeveloperIconProps} from '../createDeveloperIcon';\nexport const ${iconName} = (props: DeveloperIconProps) => createElement(Icon, {...${JSON.stringify(
     svgObject.attributes
-  )}, ...props, children: [${children}]})`;
+  )}, ...props, children: [${children}]
+  })`;
 };
 
-export const Icon = (props) => {
-  const svgString = createElement("svg", props);
+export const Icon = ({
+  size = 80,
+  className,
+  color,
+  ...rest
+}: DeveloperIconProps) => {
+  const svgString = createElement("svg", {
+    ...defaultAttributes,
+    stroke: color,
+    className: mergeClassNames("developer-icons", className),
+    ...rest,
+    width: size,
+    height: size,
+  });
   return svgString;
 };
