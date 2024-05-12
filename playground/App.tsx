be4bc411b02794974ffb7b13ae2fb50460a7972b
Sandesh Katwal <dev01.sandesh@gmail.com>
2024-05-12 14:59:42 +0545

    :zap: improvement: change src folder to playground for testing developer-icons components

---
+++ playground/App.tsx
@@ -0,0 +1,28 @@
+import "./App.css";
+import { InstagramIcon, ReactIcon } from "../dist/main";
+
+function App() {
+  return (
+    <>
+      <div>
+        <ReactIcon size={150} />
+        <InstagramIcon size={150} style={{ marginLeft: 30 }} />
+      </div>
+      <h1>Developer-Icons Playground</h1>
+      <div className="card">
+        <a href="https://github.com/xandemon/developer-icons">
+          <button>github.com/xandemon/developer-icons</button>
+        </a>
+        <p>
+          Edit <code>playground/App.tsx</code> and save to see changes
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Test around with <i>developer-icons</i> components here while developing
+        them
+      </p>
+    </>
+  );
+}
+
+export default App;
