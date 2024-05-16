13a2a8aae037411bd10a34b77ebd488c66bde20b
Sandesh Katwal <dev01.sandesh@gmail.com>
2024-05-16 14:24:59 +0545

    docs: :wrench: config: github action config for astro project

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -5,6 +5,8 @@ import tailwind from "@astrojs/tailwind";
 
 // https://astro.build/config
 export default defineConfig({
+  site: "https://xandemon.github.io",
+  base: "/developer-icons/docs",
   integrations: [
     react(),
     tailwind({
