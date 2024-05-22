e62af7cb6889b0194c5769a49877e96673fdfc1f
Sandesh Katwal <dev01.sandesh@gmail.com>
2024-05-22 23:10:18 +0545

    docs: :zap: improvement: use env for static assets not loading in github pages issue

--- docs/src/lib/utils.ts
+++ docs/src/lib/utils.ts
@@ -1,6 +1,8 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { twMerge } from "tailwind-merge";
+
+export const publicBaseUrl = import.meta.env.VITE_PUBLIC_BASE_URL;
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
