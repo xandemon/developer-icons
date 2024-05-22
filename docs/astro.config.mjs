705643a79d16259934a42e928a485185bc898651
Sandesh Katwal <dev01.sandesh@gmail.com>
2024-05-22 22:18:36 +0545

    docs: fix css and assets not loading issue

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -7,6 +7,7 @@ import tailwind from "@astrojs/tailwind";
 export default defineConfig({
   site: "https://xandemon.github.io",
   base: "/developer-icons/docs",
+  publicDir: "/developer-icons/docs/assets",
   integrations: [
     react(),
     tailwind({
