48c84a12dcabedabe89cc2cdbdd53c494716d7ef
xandemon <dev01.sandesh@gmail.com>
2024-05-29 23:32:33 +0545

    :star: feat: add unique ids with filename prefix in svg to avoid id collisions and unexpected rendering issue

--- lib/createDeveloperIcon.ts
+++ lib/createDeveloperIcon.ts
@@ -1,12 +1,23 @@
 import { parseSync, stringify } from "svgson";
 import { optimize } from "svgo";
+import { capitalizeFirstletter } from "./utils";
 
 export const createDeveloperIcon = (
   iconName: string,
   iconContent: string,
   path: string
 ) => {
-  const optimizedSvg = optimize(iconContent, { path }).data;
+  const optimizedSvg = optimize(iconContent, {
+    path,
+    plugins: [
+      {
+        name: "prefixIds",
+        params: {
+          prefix: capitalizeFirstletter(iconName, true),
+        },
+      },
+    ],
+  }).data;
   const svgObject = parseSync(optimizedSvg, { camelcase: true });
   const children = [...svgObject.children.map((child) => stringify(child))];
 
