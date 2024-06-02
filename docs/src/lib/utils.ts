f5ac0a3731fed365d7e9ce437be4c8d9d57419bb
xandemon <dev01.sandesh@gmail.com>
2024-06-02 14:23:44 +0545

    docs: :star: new: create a downloader util function to download files from url

--- docs/src/lib/utils.ts
+++ docs/src/lib/utils.ts
@@ -6,3 +6,12 @@ export const publicBaseUrl = import.meta.env.VITE_PUBLIC_BASE_URL;
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
+
+export function downloader(url: string, filename: string) {
+  const downloadEl = document.createElement("a");
+  downloadEl.href = url;
+  downloadEl.download = filename;
+  document.body.appendChild(downloadEl);
+  downloadEl.click();
+  document.body.removeChild(downloadEl);
+}
