5bfd55d61b7213a5c0a0d4db2a553e020c1713da
xandemon <dev01.sandesh@gmail.com>
2024-06-02 14:25:30 +0545

    docs: :star: new: create a loader wrapper component

---
+++ docs/src/components/ui/loader.tsx
@@ -0,0 +1,28 @@
+import { cn } from "@/lib/utils";
+import type { ReactNode } from "react";
+
+export const Loader = ({
+  loading,
+  children,
+  className = "",
+  style,
+}: {
+  loading: boolean;
+  children: ReactNode;
+  className?: string;
+  style?: React.CSSProperties;
+}) => {
+  if (loading) {
+    return (
+      <div
+        className={cn(
+          `w-5 h-5 border-[3px] border-zinc-800 border-t-zinc-600 rounded-full animate-spin`,
+          { [className]: !!className }
+        )}
+        style={style}
+      />
+    );
+  } else {
+    return children;
+  }
+};
