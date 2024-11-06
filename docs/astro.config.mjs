51971649288b9a4d7e8c7136f4c2c4f5e872ab18
xandemon <dev01.sandesh@gmail.com>
2024-11-06 23:29:40 +0545

    docs: :package: package: add astro partytown

--- docs/astro.config.mjs
+++ docs/astro.config.mjs
@@ -1,17 +1,18 @@
 import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
-
 import tailwind from "@astrojs/tailwind";
+import partytown from "@astrojs/partytown";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://xandemon.github.io",
   base: "/developer-icons/docs",
   publicDir: "./assets",
-  integrations: [
-    react(),
-    tailwind({
-      applyBaseStyles: false,
-    }),
-  ],
+  integrations: [react(), tailwind({
+    applyBaseStyles: false,
+  }), partytown({
+    config: {
+      forward: ['dataLayer.push']
+    }
+  })],
 });
