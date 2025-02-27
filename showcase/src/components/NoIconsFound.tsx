d0b8073058a894513044b1a61566341cb64de976
The-Best-Codes <bestcodes.official@gmail.com>
2025-02-27 14:08:11 -0600

    add image in assets/ for no icon found

--- showcase/src/components/NoIconsFound.tsx
+++ showcase/src/components/NoIconsFound.tsx
@@ -1,16 +1,14 @@
-import React from 'react';
-
 const NoIconsFound = () => {
   return (
     <div className="flex flex-col items-center justify-center h-full text-center">
       <img
-        src="https://www.vecteezy.com/system/resources/previews/007/104/553/non_2x/search-no-result-not-found-concept-illustration-flat-design-eps10-modern-graphic-element-for-landing-page-empty-state-ui-infographic-icon-vector.jpg"
+        src="/assets/no-icons-found.png"
         alt="No Icons Found"
-        className="w-1/2 h-auto mb-4"
+        className="max-w-48"
       />
       <h2 className="text-2xl font-bold mb-2">No Icons Found</h2>
       <p className="text-lg">
-        Looks like we don't have the icons you're looking for. You can create an{' '}
+        Looks like we don't have the icons you're looking for. You can create an{" "}
         <a
           href="https://github.com/xandemon/developer-icons/issues"
           target="_blank"
@@ -18,8 +16,8 @@ const NoIconsFound = () => {
           className="text-blue-600 hover:underline"
         >
           issue
-        </a>{' '}
-        to notify the developers or{' '}
+        </a>{" "}
+        to notify the developers or{" "}
         <a
           href="https://xandemon.github.io/developer-icons/docs/contributing/"
           target="_blank"
@@ -27,7 +25,7 @@ const NoIconsFound = () => {
           className="text-blue-600 hover:underline"
         >
           contribute
-        </a>{' '}
+        </a>{" "}
         one yourself.
       </p>
     </div>
