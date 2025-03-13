56fdae6ed226280699403353216439955563e641
mori <ys0514@yeah.net>
2025-03-13 21:02:59 +0800

    ⭐ : add deno-fresh, fast-api and hugging-face icon

--- playground/App.tsx
+++ playground/App.tsx
@@ -1,22 +1,13 @@
-import {
-  DeveloperIcons,
-  GitHubDark,
-  Instagram,
-  NextJsWordmark,
-  WebRTC,
-  Zen,
-} from "../dist/main";
+import { DenoFresh, FastAPI, HuggingFace, WebRTC } from "../dist/main";
 import "./App.css";
 
 function App() {
   return (
     <>
       <div style={{ display: "flex", alignItems: "center", gap: 30 }}>
-        <NextJsWordmark size={150} />
-        <Instagram size={150} />
-        <DeveloperIcons size={150} />
-        <Zen size={150} />
-        <GitHubDark size={150} />
+        <DenoFresh size={150} />
+        <FastAPI size={150} />
+        <HuggingFace size={150} />
         <WebRTC size={150} />
       </div>
       <h1>Developer-Icons Playground</h1>
