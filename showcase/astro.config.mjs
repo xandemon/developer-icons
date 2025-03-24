959a67161cfff1f21da6c042467b3dc8dab17101
mori <ys0514@yeah.net>
2025-03-24 20:27:22 +0800

    feature: auto build version

--- showcase/astro.config.mjs
+++ showcase/astro.config.mjs
@@ -3,11 +3,17 @@ import react from "@astrojs/react";
 import tailwind from "@astrojs/tailwind";
 import partytown from "@astrojs/partytown";
 import sitemap from "@astrojs/sitemap";
+import rootPkg from '../package.json'
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://xandemon.github.io",
   base: "/developer-icons/showcase",
+  vite:{
+    define:{
+      '__LIB_VERSION__': JSON.stringify(rootPkg.version)
+    }
+  },
   trailingSlash: "never",
   publicDir: "./assets",
   integrations: [react(), tailwind({
